2092acf264b11ce784457292e36bcc087d0d5fc0
Alejandra acevedo <urregoaleja10@gmail.com>
2020-02-04 10:14:43 +0100

    Add TP1 and new jsp page

--- src/org/mines/douai/j2ee/tp/acevedo/servlet/SimpleQuoteServlet.java
+++ src/org/mines/douai/j2ee/tp/acevedo/servlet/SimpleQuoteServlet.java
@@ -27,15 +27,40 @@ public class SimpleQuoteServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		java.io.PrintWriter out= response.getWriter();
+		this.showButton(response);
+		if(request.getParameter("conversion").equals("bitcoin")) {
+			out.print("<h2>Hello</h2></body></html>");
+		}
+		
 	}
 
+	public void showButton(HttpServletResponse rep) throws IOException {
+		rep.setContentType("text/html");
+		java.io.PrintWriter out= rep.getWriter();
+		out.print("<html><head><title>TP1</title></head>");
+		out.print("<body>"
+				+ "<select name=\"conversion\" form=\"myForm\">");
+		out.print("<option value=\"bitcoin\">Bitcoin</option>"
+				+ "<option value=\"litcoin\">Litcoin</option>"
+				+ "<option value=\"namecoin\">Namecoin</option></select>");
+		out.print( "<form action=\"http://localhost:8080/TP1/SimpleQuote\" methode=Post id='myForm'>"
+				+ "<input type=submit name=refresh value=\"refresh\"></form>");
+	}
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		if(request.getParameter("refresh") != null) {
+			doGet(request, response);
+			}
+			
+			
+		
 	}
 
 }
